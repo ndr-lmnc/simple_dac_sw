1278aa8a7517780723f2dd86c02b4195ce2dfa82
Alessandro <lomonacoalessandro@icloud.com>
2024-05-15 18:11:46 +0200

    update script with more variables

--- load.zsh
+++ load.zsh
@@ -1,2 +1,9 @@
+#!/bin/zsh
+# This script is used to flash the main.hex file to the LSD board
+# It assumes that the LSD-master folder is in the Downloads folder
+
+tty_device="/dev/tty.usbserial-DU0CQEAD"
+
 source ~/Downloads/LSD-master/.venv/bin/activate
-python3 ~/Downloads/LSD-master/lsd.py -p /dev/tty.usbserial-DM03MFHV -e -wp ./main.hex -v
+python3 ~/Downloads/LSD-master/lsd.py -p $tty_device -e -wp ./main.hex -v
+deactivate
